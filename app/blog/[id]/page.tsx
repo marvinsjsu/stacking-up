903caf6740cbc1c77f19555e1965d0f59308e033
marvinsjsu <marvinsjsu@gmail.com>
2025-01-14 13:37:44 -0800

    testing fix for iphone margin

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -62,7 +62,7 @@ export default async function Page({ params }: {
           </div>
         )}
       </main>
-      <section className="bg-white mt-10 py-20 md:mt-0">
+      <section className="bg-white mt-20 py-20 md:mt-0">
         <div
           className="prose mt-20 px-12 bg-white w-full max-w-screen-md"
           dangerouslySetInnerHTML={{ __html: htmlContent }}
